67d0c35401dfceabbb55d5eadc3d01dd0bda0d1b
AladinBrapo <jayram.celinscak@scv.si>
2024-12-25 10:59:32 +0100

    added php files and some basic structure

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer>
+    <div>
+        <span>Kontaktirajte nas:</span>
+        lanparty@scv.si <br>
+        ers_lan_scv <br>
+        Lan Party 2024
+    </div>
+    <div id="sponsors">
+
+    </div>
+</footer>
+ 
