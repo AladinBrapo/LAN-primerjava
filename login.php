a5c01924a7b4cd73a8f9fbb213ef8ea5b994d44d
AladinBrapo <jayram.celinscak@scv.si>
2025-01-22 19:54:49 +0100

    login finished

--- login.php
+++ login.php
@@ -25,18 +25,18 @@ if (isset($_POST['sub'])) {
         // Pobeg nevarnih znakov za SQL poizvedbe
         $m = mysqli_real_escape_string($link, $m);
 
-        $sql = "SELECT ime, priimek, geslo, vrsta_up_id, id FROM uporabniki WHERE email = '$m'";
+        $sql = "SELECT username, surname, password, type, id FROM students WHERE email = '$m'";
         $result = mysqli_query($link, $sql);
 
         if ($result && mysqli_num_rows($result) > 0) {
             $row = mysqli_fetch_assoc($result);
 
             // Preverjanje gesla
-            if (password_verify($p, $row['geslo'])) {
-                $_SESSION['im'] = $row['ime'];
-                $_SESSION['pr'] = $row['priimek'];
+            if (password_verify($p, $row['password'])) {
+                $_SESSION['im'] = $row['username'];
+                $_SESSION['pr'] = $row['surname'];
                 $_SESSION['log'] = true;
-                $_SESSION['vrsta_up'] = $row['vrsta_up_id'];
+                $_SESSION['vrsta_up'] = $row['type'];
                 $_SESSION['uporabnik_id'] = $row['id'];
 
                 $message = '<div class="success-msg">Login was successful.</div>';
@@ -77,20 +77,29 @@ if (isset($_POST['sub'])) {
     <?php include 'header.php'; // Header ?>
     <main class="main-container">
         <h1 class="main-title">Prijava</h1>
+
         <form method="post" action="login.php">
             <div>
-                <div><img src="img/mail.png" alt="mail-icon"></div>
                 <input type="email" name="mail" placeholder="Email" required autofocus>
+                <div><img src="img/mail.png" alt="mail-icon"></div>
             </div>
             <div>
+                <input type="password" name="geslo" id="desktop-password-field" placeholder="Geslo" required>
                 <div class="pass" onclick="togglePasswordVisibility()">
                     <img src="img/eye-closed.png" id="toggle-desktop-eye" alt="eye-icon">
                 </div>
-                <input type="password" name="geslo" id="desktop-password-field" placeholder="Password" required>
             </div>
             
             <button type="submit" name="sub" value="Prijava">Prijava</button>
         </form>
+
+        <div class="registration-link">Še nimate računa? <a href="register.php">Registriraj se zdaj.</a></div>
+
+        <?php if ($message): ?>
+            <div class="message">
+                <?php echo $message; ?>
+            </div>
+        <?php endif; ?>
     </main>
     <?php include 'footer.php'; // Footer ?>
     <script>
