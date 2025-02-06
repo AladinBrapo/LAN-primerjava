0184ae913256ca192e9b1d8a8c8b82b1b0b4177f
AladinBrapo <jayram.celinscak@scv.si>
2025-02-06 17:59:12 +0100

    footer fixed

--- footer.php
+++ footer.php
@@ -1,12 +1,31 @@
 <footer>
     <div>
-        <span>Kontaktirajte nas:</span> <br>
-        lanparty@scv.si <br>
-        ers_lan_scv <br>
-        Lan Party 2024
+        <span>Kontaktirajte nas:</span>
+        <span><img src="img/mail2.png" alt="mail img"> lanparty@scv.si</span>
+        <span><img src="img/insta.png" alt="insta img"> ers_lan_scv</span>
+        <span><img src="img/discord.png" alt="discord img"> Lan Party 2024</span>
     </div>
-    <div id="sponsors">
 
+    <div id="sponsors">
+        <a href="https://uvi.gg/sl/" target="_blank">
+            <img src="img/UVI.png" alt="UVI Logo">
+        </a>
+
+        <a href="https://mcdonalds.si/" target="_blank">
+            <img src="img/mcdonalds.png" alt="McDonalds Logo">
+        </a>
+
+        <a href="https://www.lenovo.com/si/sl/" target="_blank">
+            <img src="img/lenovo.png" alt="Lenovo Logo">
+        </a>
+
+        <a href="https://snacking.si/" target="_blank">
+            <img src="img/snacking.png" alt="snacking Logo">
+        </a>
+
+        <a href="https://balix.si/" target="_blank">
+            <img src="img/balix.png" alt="Balix Logo">
+        </a>
     </div>
 </footer>
  
